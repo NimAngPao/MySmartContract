48de0babbfaedf7ce241717471e83f6dbe15f520
Totsapon Nonsreekot <totsapon_n@BKC.CO.TH>
2018-11-06 14:31:55 +0700

    ประกาศ Contract และกำหนดค่าให้ message ตอน contract ถูกสร้าง

--- contracts/greetings.sol
+++ contracts/greetings.sol
@@ -1,2 +1,18 @@
 //solium-disable linebreak-style
 pragma solidity ^0.4.18;
+
+contract Greetings {
+    string message;
+
+    constructor() public {
+        message = "OK!";
+    }
+
+    function setGreeting(string _message) public {
+        message = _message;
+    }
+
+    function getGreeting() public view returns (string) {
+        return message;
+    }
+}
